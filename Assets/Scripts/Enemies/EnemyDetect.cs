43593149112199380c2bb9178d25aa3aba9958a8
Bino46 <atomemeteore@gmail.com>
2022-03-30 09:50:41 +0200

    Detect + move des ennemis (début)

--- Assets/Scripts/Enemies/EnemyDetect.cs
+++ Assets/Scripts/Enemies/EnemyDetect.cs
@@ -7,28 +7,20 @@ public class EnemyDetect : MonoBehaviour
     [SerializeField] float radius;
     bool detect;
     public bool otherDetect;
-    public bool mustGo;
     ContactFilter2D contact;
-    [SerializeField] float speed;
+    EnemyMove move;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        move = GetComponent<EnemyMove>();
+        StartCoroutine("DetectAround");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Collider2D detectCircle = Physics2D.OverlapCircle(transform.position, radius, 1 << 0);
-        if (detectCircle != null)
-        {
-            Debug.DrawLine(gameObject.transform.position, detectCircle.transform.position, Color.magenta, 0.5f);
-            Debug.Log(detectCircle.transform.name);
-            otherDetect = true;
-            mustGo = true;
-            CallCouroutines(detectCircle.gameObject);
-        }
+
     }
 
     IEnumerator PreventOther()
@@ -40,16 +32,21 @@ public class EnemyDetect : MonoBehaviour
             {
                 if (other.gameObject.GetComponent<EnemyDetect>() != null && other.gameObject.GetComponent<EnemyDetect>().otherDetect == false)
                 {
-
                     RaycastHit2D hit2D;
                     if (hit2D = Physics2D.Linecast(transform.position, other.transform.position))
                     {
                         if (hit2D.transform.gameObject.layer == 3)
                         {
+                            GameObject otherGO = hit2D.transform.gameObject;
+                            Debug.Log(otherGO.name);
                             Debug.DrawLine(transform.position, hit2D.transform.position, Color.cyan, 50);
-                            hit2D.transform.gameObject.GetComponent<EnemyDetect>().otherDetect = true;
-                            hit2D.transform.gameObject.GetComponent<EnemyDetect>().mustGo = true;
-                            hit2D.transform.gameObject.GetComponent<EnemyDetect>().CallCouroutines(gameObject);
+
+                            otherGO.GetComponent<EnemyDetect>().otherDetect = true;
+                            otherGO.GetComponent<EnemyDetect>().StartCoroutine("PreventOther");
+
+                            otherGO.GetComponent<EnemyMove>().mustGo = true;
+                            otherGO.GetComponent<EnemyMove>().StartCoroutine("MoveOver", otherGO);
+
                         }
                     }
                 }
@@ -62,31 +59,19 @@ public class EnemyDetect : MonoBehaviour
         yield return null;
     }
 
-    IEnumerator MoveOver(GameObject lastPos)
+    IEnumerator DetectAround()
     {
-        Vector3 posToGo = lastPos.transform.position;
-        while (mustGo == true)
+        Collider2D detectCircle = Physics2D.OverlapCircle(transform.position, radius, 1 << 0);
+        if (detectCircle != null)
         {
-            transform.position = new Vector3(Mathf.Lerp(transform.position.x, posToGo.x, Time.deltaTime * speed), Mathf.Lerp(transform.position.y, posToGo.y, Time.deltaTime *speed), 0);
-            if (Vector3.Distance(transform.position, posToGo) < 1f)
-            {
-                StartCoroutine("Reset", lastPos);
-                mustGo = false;
-            }
-            //la vitesse décroît par ennemi et ils finissent pas ne plus se déplacer tellement ils sont lents
-            yield return new WaitForSeconds(0.1f);
+            Debug.DrawLine(gameObject.transform.position, detectCircle.transform.position, Color.magenta, 0.5f);
+            Debug.Log(detectCircle.transform.name);
+            otherDetect = true;
+            move.mustGo = true;
+            move.StartCoroutine("MoveOver", detectCircle.gameObject);
+            StartCoroutine("PreventOther");
         }
-    }
-
-    IEnumerator Reset(GameObject player)
-    {
-        yield return new WaitForSeconds(0.1f);
-        player = null;
-    }
-
-    void CallCouroutines(GameObject other)
-    {
-        StartCoroutine("PreventOther");
-        StartCoroutine("MoveOver", other);
+        yield return new WaitForSeconds(1);
+        StartCoroutine("DetectAround");
     }
 }
--- Assets/Scripts/Enemies/EnemyMove.cs
+++ Assets/Scripts/Enemies/EnemyMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    public bool mustGo;
+    [SerializeField] float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +22,26 @@ public class EnemyMove : MonoBehaviour
     IEnumerator Move()
     {
         yield return null;
+    }
+
+    IEnumerator MoveOver(GameObject lastPos)
+    {
+        Vector3 posToGo = lastPos.transform.position;
+        Debug.Log(lastPos.transform.position + " " + gameObject.name);
+        while (mustGo == true)
+        {
+            transform.position = new Vector3(Mathf.Lerp(transform.position.x, posToGo.x, Time.deltaTime * speed), Mathf.Lerp(transform.position.y, posToGo.y, Time.deltaTime * speed), 0);
+            if (Vector3.Distance(transform.position, posToGo) < 1f)
+            {
+                Reset(lastPos);
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    void Reset(GameObject player)
+    {
+        player = null;
+        mustGo = false;
     }
 }
