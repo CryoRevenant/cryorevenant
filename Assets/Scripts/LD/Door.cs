b48549b30c35d0b1a6f656b52aede0fa6badc33f
Bino46 <atomemeteore@gmail.com>
2022-04-25 11:32:34 +0200

    tag door

--- Assets/Scripts/LD/Door.cs
+++ Assets/Scripts/LD/Door.cs
@@ -6,6 +6,7 @@ public class Door : MonoBehaviour
 {
     [SerializeField] GameObject doorIdle;
     [SerializeField] GameObject doorDebris;
+    [SerializeField] Transform expPos;
     // Start is called before the first frame update
     void Start()
     {
