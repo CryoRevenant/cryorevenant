3bc530f6a09b095f6e8fa9dde28a26b522a593dc
Bino46 <atomemeteore@gmail.com>
2022-04-26 14:07:40 +0200

    Ascenceur fini

--- Assets/Scripts/LD/Ascenceur.cs
+++ Assets/Scripts/LD/Ascenceur.cs
@@ -1,23 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Ascenceur : MonoBehaviour
 {
     List<GameObject> enemyList = new List<GameObject>();
     [SerializeField] GameObject otherElevator;
+
+    bool isOpen;
+    public bool isIn;
+
+    private PlayerInput controls = null;
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.Find("Player");
+        controls = player.GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (controls != null)
+        {
+            if (controls.currentActionMap.FindAction("Interact").triggered && isIn == true)
+            {
+                TPPlayer();
+                isIn = false;
+            }
+        }
     }
 
+    void CheckOpen()
+    {
+        if (enemyList.Count == 0)
+        {
+            Debug.Log(gameObject.name + " is open");
+            isOpen = true;
+        }
+    }
+
+    void TPPlayer()
+    {
+        player.transform.position = otherElevator.transform.position;
+    }
+
+    #region Add/Remove enemyList
     public void RemoveEnemy(GameObject enemy)
     {
         enemyList.Remove(enemy);
@@ -28,12 +57,26 @@ public class Ascenceur : MonoBehaviour
     {
         enemyList.Add(enemy);
     }
+    #endregion
 
-    void CheckOpen()
+    #region Triggers
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (enemyList.Count == 0)
+        if (other.gameObject.CompareTag("Player") && isOpen == true)
         {
-            Debug.Log(gameObject.name + " is open");
+            isIn = true;
+            player.GetComponent<PlayerAttack>().enabled = false;
         }
     }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.GetComponent<PlayerAttack>().enabled = true;
+            //isIn = false;
+        }
+    }
+    #endregion
 }
