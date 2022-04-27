9eaea6d7067244e86e1e4d523878a577c7b951d8
Bino46 <atomemeteore@gmail.com>
2022-04-27 09:31:40 +0200

    fuzebox terminée

--- Assets/Scripts/LD/FuzeBox.cs
+++ Assets/Scripts/LD/FuzeBox.cs
@@ -1,23 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class FuzeBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Animator animator;
+    [SerializeField] CinemachineVirtualCamera vcam;
+
+    bool isDestroyed;
+
+    GameObject player;
+
+    private void Awake()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        player = GameObject.Find("Player");
     }
 
     public void DestoyFuze()
     {
-        
+        if (isDestroyed == false)
+        {
+            vcam.m_Priority = 40;
+
+            Invoke("Anim", 0.15f);
+            Invoke("ChangeCam", 2f);
+
+            player.GetComponent<PlayerAttack>().enabled = false;
+            player.GetComponent<PlayerControllerV2>().enabled = false;
+
+            isDestroyed = true;
+        }
+    }
+
+    void Anim()
+    {
+        animator.SetTrigger("Fall");
+    }
+
+    void ChangeCam()
+    {
+        vcam.m_Priority = -40;
+
+        player.GetComponent<PlayerAttack>().enabled = true;
+        player.GetComponent<PlayerControllerV2>().enabled = true;
     }
 }
