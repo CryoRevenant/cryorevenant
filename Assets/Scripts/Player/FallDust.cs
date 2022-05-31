6fde48ac0ab33d867bb8681ca8222666615a67cf
Bino46 <94697237+Bino46@users.noreply.github.com>
2022-05-31 14:34:44 +0200

    fall dust

---
+++ Assets/Scripts/Player/FallDust.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDust : MonoBehaviour
+{
+    [SerializeField] ParticleSystem fallDust;
+    [SerializeField] float dist;
+    [SerializeField] bool isGrounded = false;
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, Vector3.down * dist, Color.blue, 0.2f);
+        RaycastHit2D hit2D;
+        if (hit2D = Physics2D.Raycast(transform.position, Vector2.down, dist))
+        {
+            if (hit2D.collider.CompareTag("Ground"))
+            {
+                if (!isGrounded)
+                {
+                    isGrounded = true;
+                    fallDust.Emit(1);
+                }
+            }
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
