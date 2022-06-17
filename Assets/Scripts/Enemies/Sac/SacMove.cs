ca1e110c8860fe3596ceb0175e5b9b5af13ba90e
Bino46 <94697237+Bino46@users.noreply.github.com>
2022-06-17 15:06:01 +0200

    sac modif début

--- Assets/Scripts/Enemies/Sac/SacMove.cs
+++ Assets/Scripts/Enemies/Sac/SacMove.cs
@@ -8,6 +8,8 @@ public class SacMove : EnemyMove
 
     [SerializeField] float overrideSpeed;
     [SerializeField] float baseSpeed;
+    [SerializeField] float timerBeforeAttack;
+
     public float speedMove;
 
     public bool canCall = true;
@@ -54,18 +56,28 @@ public class SacMove : EnemyMove
 
     IEnumerator AttackMove(GameObject lastPos)
     {
+        //GetComponentInChildren<HitSac>().CubeOn();
+        float timer = timerBeforeAttack;
         Vector3 posToGo = lastPos.transform.position;
 
         speedMove = overrideSpeed;
         canCall = false;
 
-        attak.Attack();
-
         //Tant que l'ennemi doit bouger
         while (Vector3.Distance(transform.position, posToGo) > maxStoppingDist)
         {
+            timer -= 0.1f;
+            Debug.Log(timer);
+
+            if (timer <= 0)
+            {
+                attak.Attack();
+            }
+
             Debug.DrawRay(transform.position, new Vector2(posToGo.x - transform.position.x, posToGo.y - transform.position.y), Color.yellow, 0.5f);
+
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(posToGo.x, transform.position.y, 0), Time.deltaTime * speedMove);
+
             yield return new WaitForSeconds(0.01f);
         }
         EndAttack();
@@ -74,6 +86,7 @@ public class SacMove : EnemyMove
     public void EndAttack()
     {
         StopCoroutine("AttackMove");
+        //GetComponentInChildren<HitSac>().CubeOff();
         canCall = true;
         speedMove = baseSpeed;
         attak.StopAttack();
